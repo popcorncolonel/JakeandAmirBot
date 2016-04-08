eb8fdcda2d8175ec72b0933e16dc49d636669d20
Eric Bailey <popcorncolonel@gmail.com>
2016-04-07 23:50:06 -0400

    initial work done on LNH

--- lonely.py
+++ lonely.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+
+import re
 import sys
 import time
 
@@ -11,45 +14,76 @@ from jjkae_tools import replace_top_sticky
 
 html_parser = HTMLParser.HTMLParser()
 
+python_3 = False
+if sys.version_info >= (3, 0):
+    python_3 = True
+    from bs4 import BeautifulSoup
+else:
+    from BeautifulSoup import BeautifulSoup
+
+
 class LNH:
-    def __init__(self, title, number, url, reddit_title, monthstring, reddit_url):
-        self.title = title
-        self.number = number
-        self.url = url
+    def __init__(self, titles, urls, reddit_title, monthstring, duration, reddit_url=None, desc=None):
+        self.titles = titles
+        self.urls = urls
         self.reddit_title = reddit_title
         self.monthstring = monthstring
         self.reddit_url = reddit_url
+        self.duration = duration
+        self.desc = desc
 
     def post(self, mod_info):
         post_lnh(self, mod_info)
 
+    def __repr__(self):
+        return "LNH submission object: {0}".format(self.titles)
+
+    def __str__(self):
+        return self.__repr__()
+
 
 def get_lnh_info(depth=0):
+    # type: (int) -> LNH
     monthstring = history.this_monthstring()
 
-    r = requests.get('http://vimeo.com/???', timeout=10)
+    r = requests.get('https://vimeo.com/ondemand/lonelyandhorny', timeout=10)
     html_body = r.text
 
     # Parse most recent episode info
 
+    if python_3:
+        soup = BeautifulSoup(''.join(r.text), "html.parser")
+    else:
+        soup = BeautifulSoup(''.join(r.text))
+    episode_part = soup.findAll('a', {'data-detail-url': re.compile('.*')})[0]
+    name = episode_part['title']  # "E=mc2"
+    url = episode_part['href']  # /ondemand/lonelyandhorny/<ID>
+    url = 'https://vimeo.com' + url
+    duration = soup.findAll('time')[0].contents[0]  # "08:46"
+
+    # TODO: Get most recent and second most recent, return a tuple. (not the first and second episodes)
+
     reddit_title = 'title'
     reddit_title = html_parser.unescape(reddit_title)
-    lnh_url = 'x.com'
+    lnh_url = url
     lnh_url = html_parser.unescape(lnh_url)
-    title = "test"
-    title = html_parser.unescape(title)
-    number = 1
-    reddit_url = 'reddit.com'
+    lnh_urls = (lnh_url,)
 
-    lnh_obj = LNH(title=title, number=number, url=lnh_url,
-                  reddit_title=reddit_title,
-                  monthstring=monthstring, reddit_url=reddit_url)
+    title = name
+    title = html_parser.unescape(title)
+
+    titles = (title,)
+
+    desc = 'i hope u like the show'
+
+    lnh_obj = LNH(titles=titles, urls=lnh_urls, reddit_title=reddit_title, monthstring=monthstring, duration=duration, desc=desc)
     return lnh_obj
+
 
 def check_lnh_and_post_if_new(mod_info, force_submit=False):
     lnh_obj = get_lnh_info()
     if not force_submit:
-        if ('LNH', lnh_obj.number) in mod_info.foundlist: # if episode found before
+        if ('LNH', lnh_obj.titles) in mod_info.foundlist:  # if episode found before
             return
     while True:
         try:
@@ -64,14 +98,20 @@ def check_lnh_and_post_if_new(mod_info, force_submit=False):
         except Exception as e:
             print("Error", e)
             break
-    mod_info.foundlist.append(('LNH', lnh_obj.number))
+    mod_info.foundlist.append(('LNH', lnh_obj.titles))
+
 
 def post_lnh(lnh_obj, mod_info):
+    """
+
+    :type mod_info: ModInfo
+    :type lnh_obj: LNH
+    """
     subreddit = 'jakeandamir'
     sub = mod_info.r.get_subreddit(subreddit)
     mod_info.login()
     try:
-        submission = mod_info.r.submit('jakeandamir', lnh_obj.reddit_title, url=lnh_obj.url)
+        submission = mod_info.r.submit('jakeandamir', lnh_obj.reddit_title, text=lnh_obj.desc)
     except praw.errors.AlreadySubmitted as e:
         print(e)
         lnh_obj.reddit_url = 'TODO: Get the real submitted object'
@@ -95,6 +135,7 @@ def post_lnh(lnh_obj, mod_info):
 
 def get_comment_text(lnh_obj):
     return ':)'
+
 
 def post_subreddit_comment(submission, lnh_obj):
     while True:
