fed61dc1fc8e3774ea4c34cbf8cad75e62e1bd26
Eric Bailey <popcorncolonel@gmail.com>
2017-01-16 15:02:32 -0500

    made j&abot more robust

--- jjkae.py
+++ jjkae.py
@@ -68,26 +68,40 @@ def main():
         print('Previous episode:', episodes[mod_info.next_episode - 2])
         print('Next episode to be posted:', episodes[mod_info.next_episode - 1])
 
+    past_exception_string = None
     while True:
-        iiwy_loop(mod_info, force_submit_iiwy)
-        force_submit_iiwy = False  # Only do it once
+        try:
+            iiwy_loop(mod_info, force_submit_iiwy)
+            force_submit_iiwy = False  # Only do it once
 
-        mod_loop(mod_info, force_submit_rewatch)
-        force_submit_rewatch = False  # Only do it once
+            mod_loop(mod_info, force_submit_rewatch)
+            force_submit_rewatch = False  # Only do it once
 
-        if mod_info.i % 5 == 1:
-            # do it 20% of the time. This is because Youtube pretty heavily rate limits requests, so we can't be
-            # hitting the server 12 times a minute.
-            gtd_loop(mod_info, force_submit_gtd)
-            force_submit_gtd = False  # Only do it once
+            if mod_info.i % 5 == 1:
+                # do it 20% of the time. This is because Youtube pretty heavily rate limits requests, so we can't be
+                # hitting the server 12 times a minute.
+                gtd_loop(mod_info, force_submit_gtd)
+                force_submit_gtd = False  # Only do it once
 
-        timeout = get_timeout(default_timeout)
+            timeout = get_timeout(default_timeout)
 
-        if timeout != 0:
-            time.sleep(timeout)
+            if timeout != 0:
+                time.sleep(timeout)
 
-        jjkae_tools.printinfo(mod_info)
-        mod_info.i += 1
+            jjkae_tools.printinfo(mod_info)
+            mod_info.i += 1
+        except (SystemExit, KeyboardInterrupt) as e:
+            raise e
+        except Exception as e:
+            this_exception_string = "{} {}".format(type(e).__name__, str(e.args))
+            print(this_exception_string)
+            if past_exception_string != this_exception_string:
+                jjkae_tools.send_email(subject="JAKE AND AMIR BOT ERROR", body=this_exception_string, to="popcorncolonel@gmail.com")
+                print("SENT EMAIL!!!")
+            print(this_exception_string)
+            past_exception_string = this_exception_string
+            time.sleep(1.0)
+            pass
 
 
 if __name__ == "__main__":
