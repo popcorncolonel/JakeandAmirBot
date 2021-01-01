44f56306f4e1b5a73ed5822cac08de33bc4d11d0
Eric Bailey <popcorncolonel@gmail.com>
2021-01-01 18:38:39 -0500

    move bcc to to: list for email

--- jjkae_tools.py
+++ jjkae_tools.py
@@ -24,7 +24,7 @@ def send_email(subject, body, to, bcc_list=None):
         params = {
                 'api_user': reddit_password.get_sendgrid_username(),
                 'api_key': reddit_password.get_sendgrid_password(),
-                'to[]':to,
+                'to[]':to, # this also works as a list :-) https://stackoverflow.com/questions/23384230/how-to-post-multiple-value-with-same-key-in-python-requests
                 'bcc[]':bcc_list,
                 'subject': subject,
                 'text': body,
@@ -40,11 +40,14 @@ def send_rewatch_email(permalink, title):
     """
     List of emails to notify for the subreddit rewatch
     """
-    email_list = ['popcorncolonel@gmail.com', 'jenna.n.friedman@gmail.com']
+    email_list = [
+        'popcorncolonel' '@' 'gmail.com',
+        'jenna.n.friedman' '@' 'gmail.com',
+        'cmey63' '@' 'gmail.com',
+    ]
     send_email(subject=u'Jake and Amir Subreddit Rewatch: {}'.format(title),
                body=permalink,
-               to='cmey63@gmail.com',
-               bcc_list=email_list)
+               to=email_list)
 
 
 def isnum(s):
